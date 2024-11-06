665bc40ee6d64f45b05ec00bccf3558e427c8222
Felicity Semetey <felicitysemetey628@gmail.com>
2024-11-06 16:11:50 +0000

    new

--- routes/user-route.js
+++ routes/user-route.js
@@ -9,9 +9,9 @@ userRouter.post('/users/register', registerUser);
 
 userRouter.post('/users/login', loginUser)
 
-userRouter.patch('/users/me', isAuthenticated, userProfileUpdate);//this is for single user/ logged in user
+userRouter.patch('/users/me', isAuthenticated, userProfileUpload.single('profilePicture'), userProfileUpdate);   //this is for single user/ logged in user
 
-userRouter.get('/users/me', isAuthenticated, userProfileUpload.single('profilePicture'), getUserProfile);//this is for single user/ logged in user
+userRouter.get('/users/me', isAuthenticated,  getUserProfile);//this is for single user/ logged in user
 
 // add get user profile for all users?? we want to get only peer therapists or pro therapist
 
